885ee4bb621307c7e606b0da6fbbb231bd744a63
user <ersh0ff.il@yandex.ru>
2025-08-05 02:20:02 +0300

    skyshop

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,30 @@
+package org.skypro.skyshop;
+
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.Product;
+
+public class App {
+    public static void main(String[] args) {
+        ProductBasket basket = new ProductBasket();
+
+        basket.addProduct(new Product("Молоко", 25));
+        basket.addProduct(new Product("Хлеб", 12));
+        basket.addProduct(new Product("Сыр", 40));
+        basket.addProduct(new Product("Яблоки", 30));
+        basket.addProduct(new Product("Яйца", 28));
+
+        basket.addProduct(new Product("Сок", 20));
+
+        basket.printProductBasket();
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        System.out.println(basket.checkProduct("Мороженое"));
+        System.out.println(basket.checkProduct("Яблоко"));
+
+        System.out.println("---------------------------------------------------------------------------------------");
+        basket.clearBasket();
+        basket.printProductBasket();
+        System.out.println("в корзине пусто");
+        System.out.println("---------------------------------------------------------------------------------------");
+    }
+}

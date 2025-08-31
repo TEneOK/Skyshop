2ac5173899f0c099cc0cb64f8629133c1535759d
user <ersh0ff.il@yandex.ru>
2025-08-31 03:57:09 +0300

    checkSearch update

--- src/org/skypro/skyshop/searchbar/SearchEngine.java
+++ src/org/skypro/skyshop/searchbar/SearchEngine.java
@@ -37,4 +37,36 @@ public class SearchEngine {
         }
         return results;
     }
+
+    public Searchable checkSearch(String search) throws BestResultNotFound {
+        Searchable bestVariant = null;
+        int maxCount = 0;
+
+        for (int i = 0; i < size; i++) {
+            String term = items[i].searchTerm();
+            int count = countOccurrences(term, search);
+            if (count > maxCount) {
+                maxCount = count;
+                bestVariant = items[i];
+            }
+        }
+
+        if (bestVariant == null) {
+            throw new BestResultNotFound(search);
+        }
+
+        return bestVariant;
+    }
+
+    private int countOccurrences(String text, String search) {
+        int count = 0;
+        int index = 0;
+
+        while ((index = text.indexOf(search, index)) != -1) {
+            count++;
+            index += search.length(); // двигаем индекс дальше
+        }
+
+        return count;
+    }
 }
